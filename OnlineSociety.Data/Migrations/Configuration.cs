7b7af3554273c893014453bb85b441198547bb6d
Duc Vu <de.yd315@gmail.com>
2019-11-03 17:00:32 -0600

    created db migrations

---
+++ OnlineSociety.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineSociety.Model.Migrations
+{
+    using OnlineSociety.Data;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineSocietyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OnlineSocietyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- OnlineSociety.Model/Classes.cs
+++ OnlineSociety.Model/Classes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineSociety.Classes
 {
@@ -19,6 +20,9 @@ namespace OnlineSociety.Classes
         public int Id { get; set; }
         public string Name { get; set; }
         public PetType Type { get; set; }
+        [Required]
+        public User Owner { get; set; }
+        //public int UserId { get; set; }
     }
 
     public class Clan
@@ -26,7 +30,6 @@ namespace OnlineSociety.Classes
         public int Id { get; set; }
         public string Name { get; set; }
         public List<User> Members { get; set; }
-
     }
 
     public enum PetType
